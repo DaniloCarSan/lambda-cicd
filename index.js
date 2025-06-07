45d7e2783ea75b0938aec5fe23ffd4de63eb0b04
DaniloCarSan <danilocarsan@gmail.com>
2025-06-06 22:00:46 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify('Como configurar uma pipeline CI-CD para AWS Lambda'),
+    body: JSON.stringify('FUNCIONOU !!'),
   };
 };
